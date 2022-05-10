be9cbf97892f46682e69baa8c268397f0c1eabc1
fabiokleis <fabiohkrc@gmail.com>
2022-05-09 22:52:36 -0300

    exemplo com mutex

--- ex3/include/thread.hpp
+++ ex3/include/thread.hpp
@@ -4,23 +4,30 @@ using namespace std;
 
 pthread_t thread_1;
 pthread_t thread_2;
+pthread_mutex_t meu_mutex;
 
 // funcao passada para thread_1;
 void *tarefa_1(void *p) {
     // REGIAO CRITICA
+    pthread_mutex_lock(&meu_mutex);
     string s = "123456";
     for (auto& c : s) {
         cout << c;
     }
+    pthread_mutex_unlock(&meu_mutex);
     pthread_exit(&thread_1);
 }
 
 // funcao passada para a thread_2;
 void *tarefa_2(void *p) {
     // REGIAO CRITICA
+    pthread_mutex_lock(&meu_mutex);
+
     string s = "ABCDEF";
     for (auto& c : s) {
         cout << c;
     }
+    pthread_mutex_unlock(&meu_mutex);
     pthread_exit(&thread_2);
+
 }
--- ex3/src/main.cpp
+++ ex3/src/main.cpp
@@ -7,7 +7,8 @@ int main(void) {
     cout << "Digite ENTER para iniciar a thread...."  << endl;
 
     cin.get(); // aguarda um ENTER do user;
-               //
+    
+    pthread_mutex_init(&meu_mutex, NULL);
     // inicia uma nova thread, passando como
     // parametro a thread e a funcao que se deseja
     // executar;
@@ -15,8 +16,10 @@ int main(void) {
     pthread_create(&thread_2, NULL, tarefa_2, NULL);
 
     // faz com que a thread principal espere a thread_1 e a thread_2 acabarem;
-    pthread_join(thread_1, NULL);
-    pthread_join(thread_2, NULL);
+//    pthread_join(thread_1, NULL);
+//    pthread_join(thread_2, NULL);
+
+    pthread_mutex_destroy(&meu_mutex);
 
     cin.get();
     
