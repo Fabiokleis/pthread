f24d5593098d6df415c3008cae0ec577c8b1482f
fabiokleis <fabiohkrc@gmail.com>
2022-05-09 23:09:33 -0300

    exemplo de mutex mais complexo

---
+++ ex4/include/thread.hpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <pthread.h>
+using namespace std;
+
+struct limite {
+    int baixo;
+    int alto;
+};
+
+struct limite limite_1, limite_2;
+
+pthread_t thread_1;
+pthread_t thread_2;
+pthread_mutex_t meu_mutex;
+
+
+void *encontre_primos(void *param) {
+    struct limite *aux;
+    aux = (struct limite *) param;
+    int n, k;
+    int primo;
+
+    for (n = aux->baixo; n <= aux->alto; n++) {
+        primo = 1;
+        for (k = 2; k < n; k++) {
+            if (n % 2 == 0) {
+                primo = 0;
+                break;
+            }
+        }
+
+        if (primo) {
+            pthread_mutex_lock(&meu_mutex);
+            printf(" %u ", n);
+            printf("\n");
+            pthread_mutex_unlock(&meu_mutex);
+        }
+    }
+    pthread_exit(NULL);
+}
+
+// funcao passada para thread_1;
+void *tarefa_1(void *p) {
+    // REGIAO CRITICA
+    pthread_mutex_lock(&meu_mutex);
+    string s = "123456";
+    for (auto& c : s) {
+        cout << c;
+    }
+    pthread_mutex_unlock(&meu_mutex);
+    pthread_exit(&thread_1);
+}
+
+// funcao passada para a thread_2;
+void *tarefa_2(void *p) {
+    // REGIAO CRITICA
+    pthread_mutex_lock(&meu_mutex);
+
+    string s = "ABCDEF";
+    for (auto& c : s) {
+        cout << c;
+    }
+    pthread_mutex_unlock(&meu_mutex);
+    pthread_exit(&thread_2);
+
+}
---
+++ ex4/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <pthread.h>
+#include "thread.hpp"
+using namespace std;
+
+int main(void) {
+    pthread_t thd1, thd2;
+    pthread_mutex_init(&meu_mutex, NULL);      
+    limite_1.baixo = 0;
+    limite_1.alto = 1000;
+    pthread_create(&thd1, NULL, encontre_primos, &limite_1);
+    limite_2.baixo = 1000;
+    limite_2.alto = 2000;
+    pthread_create(&thd2, NULL, encontre_primos, &limite_2);
+    pthread_join(thd1, NULL);
+    pthread_join(thd2, NULL);
+    pthread_mutex_destroy(&meu_mutex);
+
+    cin.get();
+    return 0;
+}
